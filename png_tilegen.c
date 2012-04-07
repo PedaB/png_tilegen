f4f68d73086dad4ab1e9924ef286801a153f508e
Peter Barth <osm-tilegen@won2.de>
2012-04-08 01:01:08 +0200

    add support for aspect ratio of 2:1

--- png_tilegen.c
+++ png_tilegen.c
@@ -18,7 +18,7 @@ typedef struct rgb_ {
 } rgb_t;
 
 typedef struct img_ {
-  int size;
+  int sizeX, sizeY;
   int tileX, tileY;
   rgb_t *raw_data;
 } img_t;
@@ -66,12 +66,13 @@ static int read_ppm(const char *file, img_t *img) {
   max = read_int(fp);
 
   if (max > 255) return 6;
-  if ((w != h) || !isPowOfTwo(w)) return 7;
+  if (!isPowOfTwo(h) || !isPowOfTwo(w)) return 7;
 
-  img->size = w;
-  img->raw_data = malloc(w*w*sizeof(rgb_t));
+  img->sizeX = w;
+  img->sizeY = h;
+  img->raw_data = malloc(w*h*sizeof(rgb_t));
 
-  fread(img->raw_data, sizeof(rgb_t), w*w, fp);
+  fread(img->raw_data, sizeof(rgb_t), w*h, fp);
 
   return 0;
 }
@@ -111,9 +112,10 @@ static int read_png(const char *file, img_t *img) {
 
   png_uint_32 width = png_get_image_width(png_ptr, info_ptr);
   png_uint_32 height = png_get_image_height(png_ptr, info_ptr);
-  if ((width != height) || !isPowOfTwo(width)) return 7;
+  if (!isPowOfTwo(height) || !isPowOfTwo(width)) return 7;
   
-  img->size = width;
+  img->sizeX = width;
+  img->sizeY = height;
   
   //png_uint_32 bitdepth   = png_get_bit_depth(png_ptr, info_ptr);
   png_uint_32 channels   = png_get_channels(png_ptr, info_ptr);
@@ -130,9 +132,9 @@ static int read_png(const char *file, img_t *img) {
   png_bytepp row_pointers = png_get_rows(png_ptr, info_ptr);
 
   // allocate data needed to store the image data
-  img->raw_data = malloc(width*width*sizeof(rgb_t));
+  img->raw_data = malloc(height*width*sizeof(rgb_t));
 
-  for (i = 0; i < width; i++) {
+  for (i = 0; i < height; i++) {
     memcpy(&img->raw_data[i*width], row_pointers[i], width*sizeof(rgb_t));
   }
   
@@ -142,7 +144,7 @@ static int read_png(const char *file, img_t *img) {
   return 0;
 }
 
-static int open_png_writer(const char *file, png_t *png) {
+static int open_png_writer(const char *file, png_t *png, int sizeY) {
   
   png->fp = fopen(file, "wb");
   if (!png->fp) return 2;
@@ -165,7 +167,7 @@ static int open_png_writer(const char *file, png_t *png) {
   //png_set_compression_level(png->png_ptr, Z_BEST_SPEED);
 
   png_set_IHDR(png->png_ptr, png->info_ptr,
-               TILE_SIZE, TILE_SIZE,
+               TILE_SIZE, sizeY,
                8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE, 
                PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
   
@@ -187,7 +189,7 @@ static void crop_image(img_t *img, int zoom, const char *out_dir) {
   int ret, i, u, v;
   int tileX, tileY;
   char filename[1024];
-  int nr = img->size / TILE_SIZE;
+  int nr = img->sizeX / TILE_SIZE;
   png_t writer[nr];
   png_bytep row_ptr;
 
@@ -207,15 +209,15 @@ static void crop_image(img_t *img, int zoom, const char *out_dir) {
     // setup writers
     for (u = 0; u < nr; u++) {
       snprintf(filename, 1024, "%s/%d/%d/%d.png", out_dir, zoom, tileX+u, tileY);
-      ret = open_png_writer(filename, &writer[u]);
+      ret = open_png_writer(filename, &writer[u], TILE_SIZE/(img->sizeX/img->sizeY));
       if (ret) {
         printf("Could not open file \"%s\" for writing! %d\n", filename, ret);
         exit(ret);
       }
     }
     
-    for (v = 0; v < TILE_SIZE; v++) {
-      row_ptr = (png_bytep) &img->raw_data[(i*TILE_SIZE+v)*img->size];
+    for (v = 0; v < img->sizeY/nr; v++) {
+      row_ptr = (png_bytep) &img->raw_data[(i*(img->sizeY/nr)+v)*img->sizeX];
       for (u = 0; u < nr; u++) {
         png_write_row(writer[u].png_ptr, row_ptr);
         row_ptr += sizeof(rgb_t) * TILE_SIZE;
@@ -231,8 +233,8 @@ static void crop_image(img_t *img, int zoom, const char *out_dir) {
 static void scale_image(img_t *img) {
 
   int u, v;
-  int size = img->size;
-  int new_size = size/2;
+  const int sizeX = img->sizeX;
+  const int sizeY = img->sizeY;
 
   /* 
    * This code code be nicely optimized with sse.
@@ -240,13 +242,13 @@ static void scale_image(img_t *img) {
    * has <<1% cpu time
    */
  
-  for (v = 0; v < new_size; v++) {
-    for (u = 0; u < new_size; u++) {
+  for (v = 0; v < sizeY/2; v++) {
+    for (u = 0; u < sizeX/2; u++) {
         
-       rgb_t v0 = img->raw_data[2*u+2*v*size];
-       rgb_t v1 = img->raw_data[2*u+1+2*v*size];
-       rgb_t v2 = img->raw_data[2*u+(2*v+1)*size];
-       rgb_t v3 = img->raw_data[2*u+1+(2*v+1)*size];
+       rgb_t v0 = img->raw_data[2*u+2*v*sizeX];
+       rgb_t v1 = img->raw_data[2*u+1+2*v*sizeX];
+       rgb_t v2 = img->raw_data[2*u+(2*v+1)*sizeX];
+       rgb_t v3 = img->raw_data[2*u+1+(2*v+1)*sizeX];
        
        int r = ((int) v0.r) + v1.r + v2.r + v3.r;
        int g = ((int) v0.g) + v1.g + v2.g + v3.g;
@@ -256,11 +258,12 @@ static void scale_image(img_t *img) {
        v0.g = g/4;
        v0.b = b/4;
        
-       img->raw_data[u+v*new_size] = v0;
+       img->raw_data[u+v*sizeX/2] = v0;
     }
   }
   
-  img->size /= 2;
+  img->sizeX /= 2;
+  img->sizeY /= 2;
 }
 
 void main(int argc, char **argv) {
@@ -329,7 +332,7 @@ void main(int argc, char **argv) {
     crop_image(&img, zoom, argv[2]);
 
     // if we had only one tile left we're finished
-    if (img.size == TILE_SIZE)
+    if (img.sizeX == TILE_SIZE)
       break;
 
     scale_image(&img);

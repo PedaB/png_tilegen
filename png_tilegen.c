b58a1901a778f8971ff45c1187f5cb75d0b11740
Peter Barth <osm-tilegen@won2.de>
2012-07-07 22:50:50 +0200

    add support for jpg output

--- png_tilegen.c
+++ png_tilegen.c
@@ -10,7 +10,14 @@
 #include <zlib.h>
 #include <png.h>
 
+#include <jpeglib.h>
+
 #define TILE_SIZE 256
+
+#define PNG 0
+#define JPG 1
+
+#define QUALITY 75
 
 typedef struct rgb_ {
   unsigned char r;
@@ -24,11 +31,18 @@ typedef struct img_ {
   rgb_t *raw_data;
 } img_t;
 
+
 typedef struct png_ {
   FILE *fp;
   png_structp png_ptr;
   png_infop info_ptr;
 } png_t;
+
+typedef struct jpg_ {
+  FILE * fp;
+  struct jpeg_compress_struct cinfo;
+  struct jpeg_error_mgr jerr;
+} jpg_t;
 
 static int isPowOfTwo(int v) {
   return !(v & (v-1));
@@ -178,6 +192,27 @@ static int open_png_writer(const char *file, png_t *png, int sizeY) {
   return 0;
 }
 
+static int open_jpg_writer(const char *file, jpg_t *jpg, int sizeY) {
+
+  jpg->fp = fopen(file, "wb");
+  if(!jpg->fp) return 2;
+
+  jpg->cinfo.err = jpeg_std_error(&jpg->jerr);
+  jpeg_create_compress(&jpg->cinfo);
+  jpeg_stdio_dest(&jpg->cinfo, jpg->fp);
+
+  jpg->cinfo.image_width = TILE_SIZE;
+  jpg->cinfo.image_height = sizeY;
+  jpg->cinfo.input_components = 3;
+  jpg->cinfo.in_color_space = JCS_RGB;
+
+  jpeg_set_defaults(&jpg->cinfo);
+  jpeg_set_quality(&jpg->cinfo, QUALITY, TRUE);
+  jpeg_start_compress(&jpg->cinfo, TRUE);
+
+  return 0;
+}
+
 static void close_png_writer(png_t *png) {
 
   png_write_end(png->png_ptr, png->info_ptr);
@@ -186,14 +221,24 @@ static void close_png_writer(png_t *png) {
   fclose(png->fp);
 }
 
-static void crop_image(img_t *img, int zoom, const char *out_dir) {
+static void close_jpg_writer(jpg_t *jpg) {
+
+  jpeg_finish_compress(&jpg->cinfo);
+  jpeg_destroy_compress(&jpg->cinfo);
+
+  fclose(jpg->fp);
+}
+
+static void crop_image(img_t *img, int zoom, const char *out_dir, int output_format) {
   
   int ret, i, u, v;
   int tileX, tileY;
   char filename[1024];
   int nr = img->sizeX / TILE_SIZE;
-  png_t writer[nr];
-  png_bytep row_ptr;
+  png_t png_writer[nr];
+  jpg_t jpg_writer[nr];
+  png_bytep png_row_ptr;
+  JSAMPROW jpg_row_ptr[1];
 
   tileX = img->tileX * nr;
 
@@ -210,8 +255,16 @@ static void crop_image(img_t *img, int zoom, const char *out_dir) {
     
     // setup writers
     for (u = 0; u < nr; u++) {
-      snprintf(filename, 1024, "%s/%d/%d/%d.png", out_dir, zoom, tileX+u, tileY);
-      ret = open_png_writer(filename, &writer[u], TILE_SIZE/(img->sizeX/img->sizeY));
+      switch(output_format) {
+      case PNG:
+	snprintf(filename, 1024, "%s/%d/%d/%d.png", out_dir, zoom, tileX+u, tileY);
+	ret = open_png_writer(filename, &png_writer[u], TILE_SIZE/(img->sizeX/img->sizeY));
+	break;
+      case JPG:
+	snprintf(filename, 1024, "%s/%d/%d/%d.jpg", out_dir, zoom, tileX+u, tileY);
+	ret = open_jpg_writer(filename, &jpg_writer[u], TILE_SIZE/(img->sizeX/img->sizeY));
+	break;
+      }
       if (ret) {
         printf("Could not open file \"%s\" for writing! %d\n", filename, ret);
         exit(ret);
@@ -219,16 +272,36 @@ static void crop_image(img_t *img, int zoom, const char *out_dir) {
     }
     
     for (v = 0; v < img->sizeY/nr; v++) {
-      row_ptr = (png_bytep) &img->raw_data[(i*(img->sizeY/nr)+v)*img->sizeX];
-      for (u = 0; u < nr; u++) {
-        png_write_row(writer[u].png_ptr, row_ptr);
-        row_ptr += sizeof(rgb_t) * TILE_SIZE;
+
+      switch(output_format) {
+      case PNG:
+	png_row_ptr = (png_bytep) &img->raw_data[(i*(img->sizeY/nr)+v)*img->sizeX];
+	for (u = 0; u < nr; u++) {
+	  png_write_row(png_writer[u].png_ptr, png_row_ptr);
+	  png_row_ptr += sizeof(rgb_t) * TILE_SIZE;
+	}
+	break;
+      case JPG:
+	jpg_row_ptr[0] = (JSAMPROW) &img->raw_data[(i*(img->sizeY/nr)+v)*img->sizeX];
+	for (u = 0; u < nr; u++) {
+	  jpeg_write_scanlines(&jpg_writer[u].cinfo, jpg_row_ptr, 1);
+	  jpg_row_ptr[0] += sizeof(rgb_t) * TILE_SIZE;
+	}
+	break;
       }
     }
     
     // close writers
-    for (u = 0; u < nr; u++)
-      close_png_writer(&writer[u]);
+    for (u = 0; u < nr; u++) {
+      switch(output_format) {
+      case PNG:
+	close_png_writer(&png_writer[u]);
+	break;
+      case JPG:
+	close_jpg_writer(&jpg_writer[u]);
+	break;
+      }
+    }
   }
 }
 
@@ -272,11 +345,15 @@ int main(int argc, char **argv) {
 
   int ret, zoom, tmp;
   img_t img;
-  
+  int output_format = PNG;
+
   if (argc < 6) {
     printf("Usage:\ntilegen <input.png> <output_dir> <tileX> <tileY> <max_zoom> [cardinal_direction N|E|S|W]\n");
     exit(1);
   }
+
+  if (strstr(argv[0], "jpg"))
+    output_format = JPG;
   
   printf("Read file \"%s\"\n", argv[1]);
   if (strstr(argv[1], ".png"))
@@ -330,7 +407,7 @@ int main(int argc, char **argv) {
     printf("Processing zoom %d\n", zoom);
     
     // crop image at given zoom level..
-    crop_image(&img, zoom, argv[2]);
+    crop_image(&img, zoom, argv[2], output_format);
 
     // if we had only one tile left we're finished
     if (img.sizeX == TILE_SIZE)
